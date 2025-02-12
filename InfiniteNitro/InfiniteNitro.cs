14659acf4b50229476b623cd2666c46019e5023c
RunAwayBlues <172659985+RunAwayBlues@users.noreply.github.com>
2025-02-12 13:02:14 -0500

    Moved lookups to the Mod_Onload method to improve performance

--- InfiniteNitro/InfiniteNitro.cs
+++ InfiniteNitro/InfiniteNitro.cs
@@ -9,22 +9,48 @@ namespace InfiniteNitro
     public class InfiniteNitro : Mod
     {
         public override string ID => "InfiniteNitro";
-        public override string Version => "1.0";
-        public override string Author => "アカツキ";
+        public override string Version => "1.1.0";
+        public override string Author => "アカツキ/ RunAwayBlues";
+        public override string Description => "N2O (Nitrous Oxide) Bottle never depletes.";
+
+        private GameObject n2oBottle;
+        private PlayMakerFSM n2oBottlePressure;
+        private FsmFloat n2oBottlePressureFluid;
 
         public override void ModSetup()
         {
             base.ModSetup();
-            
-            SetupFunction(Setup.Update, On_Update);
+
+            SetupFunction(Setup.OnLoad, Mod_OnLoad);
+            SetupFunction(Setup.OnModDisabled, Mod_OnDisabled);
+            SetupFunction(Setup.OnModEnabled, Mod_OnEnabled);
+            SetupFunction(Setup.Update, Mod_Update);
         }
 
-        private void On_Update()
+        public void Mod_OnLoad()
         {
-            var n2o = GameObject.Find("n2o bottle(Clone)/Dynamics");
-            if (n2o != null && n2o.activeSelf)
+            n2oBottle = GameObject.Find("n2o bottle(Clone)/Dynamics");
+            n2oBottlePressure = n2oBottle.GetPlayMaker("Pressure");
+            n2oBottlePressureFluid = n2oBottlePressure.FsmVariables.GetFsmFloat("Fluid");
+
+            ModConsole.Log("Infinite Nitro Mod Applied");
+        }
+
+        public void Mod_OnEnabled()
+        {
+            ModConsole.Log("Infinite Nitrous Oxide enabled");
+        }
+
+        public void Mod_OnDisabled()
+        {
+            ModConsole.Log("Infinite Nitrous Oxide disabled");
+        }
+
+        public void Mod_Update()
+        {
+            if(!isDisabled && n2oBottle != null && n2oBottle.activeSelf)
             {
-                n2o.GetPlayMaker("Pressure").GetVariable<FsmFloat>("Fluid").Value = 5;
+                n2oBottlePressureFluid.Value = 5f;
             }
         }
     }
